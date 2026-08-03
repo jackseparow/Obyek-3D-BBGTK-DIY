90ac79ef92cc912f1aea4d5c60dbcaa8bc3f9271
jackseparow <49458990+jackseparow@users.noreply.github.com>
2026-08-03 16:38:37 +0700

    Update block_translate.js

--- block_translate.js
+++ block_translate.js
@@ -1,6 +1,3 @@
-/**
- * Custom Block: TRANSLASI (GeoBlock BBGTK DIY)
- */
 Blockly.Blocks['transform_translate'] = {
   init: function() {
     this.appendDummyInput().appendField("translasikan");
@@ -12,18 +9,14 @@ Blockly.Blocks['transform_translate'] = {
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setColour("#FF9800");
-    this.setTooltip("Menggeser posisi objek sejauh sumbu X, Y, Z");
   }
 };
 
-const genTranslate = javascript.javascriptGenerator || javascriptGenerator;
-
-genTranslate.forBlock['transform_translate'] = function(block, generator) {
-  const g = generator || genTranslate;
-  var x = g.valueToCode(block, 'X', g.ORDER_ATOMIC) || '0';
-  var y = g.valueToCode(block, 'Y', g.ORDER_ATOMIC) || '0';
-  var z = g.valueToCode(block, 'Z', g.ORDER_ATOMIC) || '0';
-  var statement = g.statementToCode(block, 'OBJECTS');
+javascript.javascriptGenerator.forBlock['transform_translate'] = function(block, generator) {
+  var x = generator.valueToCode(block, 'X', generator.ORDER_ATOMIC) || '0';
+  var y = generator.valueToCode(block, 'Y', generator.ORDER_ATOMIC) || '0';
+  var z = generator.valueToCode(block, 'Z', generator.ORDER_ATOMIC) || '0';
+  var statement = generator.statementToCode(block, 'OBJECTS');
 
   return `
 (function() {
