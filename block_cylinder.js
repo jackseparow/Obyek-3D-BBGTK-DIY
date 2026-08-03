7a4faeaa42643706d7c9ffc20d225d3e2bb37fed
jackseparow <49458990+jackseparow@users.noreply.github.com>
2026-08-03 11:49:53 +0700

    Update block_cylinder.js

--- block_cylinder.js
+++ block_cylinder.js
@@ -11,7 +11,7 @@ Blockly.Blocks['shape_cylinder'] = {
     this.appendDummyInput()
         .appendField("titik acuan")
         .appendField(new Blockly.FieldDropdown([
-          ["pusat massa", "CENTER"],
+          ["titik pusat", "CENTER"],
           ["tepi", "CORNER"]
         ]), "ALIGN");
     this.appendValueInput("POS_X").setCheck("Number").appendField("x");
