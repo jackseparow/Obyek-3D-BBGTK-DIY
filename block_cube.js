094aa1dae3cc3f648c914e763812598f80a46c0b
jackseparow <49458990+jackseparow@users.noreply.github.com>
2026-08-03 11:10:33 +0700

    Update block_cube.js

--- block_cube.js
+++ block_cube.js
@@ -1,47 +1,36 @@
 /**
- * Blok Custom 3D Shapes: KUBUS (CUBE)
+ * Custom Block: KUBUS (GeoBlock BBGTK DIY)
  */
-
-// 1. Definisi Blok
 Blockly.Blocks['shape_cube'] = {
   init: function() {
-    this.appendDummyInput()
-        .appendField("buat kubus");
     this.appendValueInput("X")
         .setCheck("Number")
-        .setAlign(Blockly.ALIGN_RIGHT)
-        .appendField("panjang X");
+        .appendField("buat kubus  panjang X");
     this.appendValueInput("Y")
         .setCheck("Number")
-        .setAlign(Blockly.ALIGN_RIGHT)
         .appendField("lebar Y");
     this.appendValueInput("Z")
         .setCheck("Number")
-        .setAlign(Blockly.ALIGN_RIGHT)
         .appendField("tinggi Z");
-    this.appendValueInput("COLOR")
-        .setCheck("Colour")
-        .setAlign(Blockly.ALIGN_RIGHT)
-        .appendField("warna");
-    this.setPreviousStatement(true, "3D_SHAPE");
-    this.setNextStatement(true, "3D_SHAPE");
+    this.setInputsInline(true); // Memanjang ke kanan
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
     this.setColour("#4C97FF");
     this.setTooltip("Membuat objek 3D Kubus / Balok");
-    this.setHelpUrl("");
   }
 };
 
-// 2. Generator Kode JavaScript (Three.js)
-javascriptGenerator.forBlock['shape_cube'] = function(block, generator) {
-  var x = generator.valueToCode(block, 'X', javascriptGenerator.ORDER_ATOMIC) || '10';
-  var y = generator.valueToCode(block, 'Y', javascriptGenerator.ORDER_ATOMIC) || '10';
-  var z = generator.valueToCode(block, 'Z', javascriptGenerator.ORDER_ATOMIC) || '10';
-  var color = generator.valueToCode(block, 'COLOR', javascriptGenerator.ORDER_ATOMIC) || "'#4c97ff'";
+const genCube = javascript.javascriptGenerator || javascriptGenerator;
+genCube.forBlock['shape_cube'] = function(block, generator) {
+  const g = generator || genCube;
+  var x = g.valueToCode(block, 'X', g.ORDER_ATOMIC) || '10';
+  var y = g.valueToCode(block, 'Y', g.ORDER_ATOMIC) || '10';
+  var z = g.valueToCode(block, 'Z', g.ORDER_ATOMIC) || '10';
 
   var code = `
 (function() {
   const geom = new THREE.BoxGeometry(${x}, ${z}, ${y});
-  const mat = new THREE.MeshStandardMaterial({ color: ${color}, roughness: 0.3 });
+  const mat = new THREE.MeshStandardMaterial({ color: 0xC0C0C0, roughness: 0.4, metalness: 0.5 });
   const mesh = new THREE.Mesh(geom, mat);
   mesh.position.set(0, ${z} / 2, 0);
   sceneGroup.add(mesh);
