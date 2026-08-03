f7fb83a171376453876410cf35b999f56b4d292b
jackseparow <49458990+jackseparow@users.noreply.github.com>
2026-08-03 12:31:51 +0700

    Update block_cylinder.js

--- block_cylinder.js
+++ block_cylinder.js
@@ -1,19 +1,10 @@
-/**
- * Custom Block: SILINDER (GeoBlock BBGTK DIY)
- */
 Blockly.Blocks['shape_cylinder'] = {
   init: function() {
-    this.appendDummyInput()
-        .appendField("buat silinder");
+    this.appendDummyInput().appendField("buat silinder");
     this.appendValueInput("RADIUS_TOP").setCheck("Number").appendField("r-atas");
     this.appendValueInput("RADIUS_BOTTOM").setCheck("Number").appendField("r-bawah");
     this.appendValueInput("HEIGHT").setCheck("Number").appendField("tinggi");
-    this.appendDummyInput()
-        .appendField("titik acuan")
-        .appendField(new Blockly.FieldDropdown([
-          ["titik pusat", "CENTER"],
-          ["tepi", "CORNER"]
-        ]), "ALIGN");
+    this.appendDummyInput().appendField("titik acuan").appendField(new Blockly.FieldDropdown([["titik pusat", "CENTER"], ["tepi", "CORNER"]]), "ALIGN");
     this.appendValueInput("POS_X").setCheck("Number").appendField("x");
     this.appendValueInput("POS_Y").setCheck("Number").appendField("y");
     this.appendValueInput("POS_Z").setCheck("Number").appendField("z");
@@ -21,10 +12,8 @@ Blockly.Blocks['shape_cylinder'] = {
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setColour("#4C97FF");
-    this.setTooltip("Membuat objek 3D Silinder atau Kerucut");
   }
 };
-
 const genCylinder = javascript.javascriptGenerator || javascriptGenerator;
 genCylinder.forBlock['shape_cylinder'] = function(block, generator) {
   const g = generator || genCylinder;
@@ -35,27 +24,19 @@ genCylinder.forBlock['shape_cylinder'] = function(block, generator) {
   var py = g.valueToCode(block, 'POS_Y', g.ORDER_ATOMIC) || '0';
   var pz = g.valueToCode(block, 'POS_Z', g.ORDER_ATOMIC) || '0';
   var align = block.getFieldValue('ALIGN');
-
-  var code = `
+  return `
 (function() {
   const geom = new THREE.CylinderGeometry(${rTop}, ${rBottom}, ${height}, 32);
+  geom.rotateX(Math.PI / 2); // Menyelaraskan tinggi silinder agar berdiri di sumbu Z
   const mat = new THREE.MeshStandardMaterial({ color: 0xC0C0C0, roughness: 0.4, metalness: 0.5 });
   const mesh = new THREE.Mesh(geom, mat);
-  
-  let finalX = Number(${px});
-  let finalY = Number(${py});
-  let finalZ = Number(${pz});
-
+  let finalX = Number(${px}), finalY = Number(${py}), finalZ = Number(${pz});
   if ("${align}" === "CORNER") {
     const maxR = Math.max(${rTop}, ${rBottom});
-    finalX += maxR;
-    finalY += ${height} / 2;
-    finalZ += maxR;
+    finalX += maxR; finalY += maxR; finalZ += ${height} / 2;
   }
-
   mesh.position.set(finalX, finalY, finalZ);
   sceneGroup.add(mesh);
 })();
 `;
-  return code;
 };
