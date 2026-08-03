588c43806d86b451f038893248b3cabe1624c7d8
jackseparow <49458990+jackseparow@users.noreply.github.com>
2026-08-03 12:33:02 +0700

    Update block_torus.js

--- block_torus.js
+++ block_torus.js
@@ -1,18 +1,9 @@
-/**
- * Custom Block: TORUS / DONAT (GeoBlock BBGTK DIY)
- */
 Blockly.Blocks['shape_torus'] = {
   init: function() {
-    this.appendDummyInput()
-        .appendField("buat torus");
+    this.appendDummyInput().appendField("buat torus");
     this.appendValueInput("RADIUS").setCheck("Number").appendField("r-utama");
     this.appendValueInput("TUBE").setCheck("Number").appendField("r-tabung");
-    this.appendDummyInput()
-        .appendField("titik acuan")
-        .appendField(new Blockly.FieldDropdown([
-          ["titik pusat", "CENTER"],
-          ["tepi", "CORNER"]
-        ]), "ALIGN");
+    this.appendDummyInput().appendField("titik acuan").appendField(new Blockly.FieldDropdown([["titik pusat", "CENTER"], ["tepi", "CORNER"]]), "ALIGN");
     this.appendValueInput("POS_X").setCheck("Number").appendField("x");
     this.appendValueInput("POS_Y").setCheck("Number").appendField("y");
     this.appendValueInput("POS_Z").setCheck("Number").appendField("z");
@@ -20,10 +11,8 @@ Blockly.Blocks['shape_torus'] = {
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setColour("#4C97FF");
-    this.setTooltip("Membuat objek 3D Torus / Donat");
   }
 };
-
 const genTorus = javascript.javascriptGenerator || javascriptGenerator;
 genTorus.forBlock['shape_torus'] = function(block, generator) {
   const g = generator || genTorus;
@@ -33,28 +22,18 @@ genTorus.forBlock['shape_torus'] = function(block, generator) {
   var py = g.valueToCode(block, 'POS_Y', g.ORDER_ATOMIC) || '0';
   var pz = g.valueToCode(block, 'POS_Z', g.ORDER_ATOMIC) || '0';
   var align = block.getFieldValue('ALIGN');
-
-  var code = `
+  return `
 (function() {
   const geom = new THREE.TorusGeometry(${radius}, ${tube}, 16, 100);
   const mat = new THREE.MeshStandardMaterial({ color: 0xC0C0C0, roughness: 0.4, metalness: 0.5 });
   const mesh = new THREE.Mesh(geom, mat);
-  mesh.rotation.x = Math.PI / 2;
-  
-  let finalX = Number(${px});
-  let finalY = Number(${py});
-  let finalZ = Number(${pz});
-
+  let finalX = Number(${px}), finalY = Number(${py}), finalZ = Number(${pz});
   if ("${align}" === "CORNER") {
     const outerR = Number(${radius}) + Number(${tube});
-    finalX += outerR;
-    finalY += Number(${tube});
-    finalZ += outerR;
+    finalX += outerR; finalY += outerR; finalZ += Number(${tube});
   }
-
   mesh.position.set(finalX, finalY, finalZ);
   sceneGroup.add(mesh);
 })();
 `;
-  return code;
 };
