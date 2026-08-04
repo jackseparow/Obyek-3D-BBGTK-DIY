e6460c17827dba0ff83a02143be6add46772e374
jackseparow <49458990+jackseparow@users.noreply.github.com>
2026-08-04 09:36:50 +0700

    Update block_intersection.js

--- block_intersection.js
+++ block_intersection.js
@@ -1,5 +1,5 @@
 /**
- * Custom Block: IRISAN (INTERSECTION)
+ * Custom Block: OPERASI HIMPUNAN - IRISAN (INTERSECTION)
  * GeoBlock BBGTK DIY
  */
 
@@ -7,28 +7,64 @@ Blockly.Blocks['csg_intersection'] = {
   init: function() {
     this.appendDummyInput()
         .appendField("irisan");
-    
-    this.appendStatementInput("OBJECTS")
-        .appendField("objek-objek");
+
+    // Slot Input Objek Pertama
+    this.appendStatementInput("OBJECTS_1")
+        .appendField("objek 1");
+
+    // Slot Input Objek Kedua (Tambahan Slot 2)
+    this.appendStatementInput("OBJECTS_2")
+        .appendField("objek 2");
 
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setColour("#E91E63");
-    this.setTooltip("Mengambil area/volume berpotongan yang saling beririsan di antara objek-objek");
+    this.setTooltip("Mengambil bagian volume yang saling berpotongan/beririsan di antara objek 1 dan objek 2");
   }
 };
 
+// Generator Kode JavaScript untuk Three.js Viewport
 javascript.javascriptGenerator.forBlock['csg_intersection'] = function(block, generator) {
-  var statement = generator.statementToCode(block, 'OBJECTS');
+  var statement1 = generator.statementToCode(block, 'OBJECTS_1');
+  var statement2 = generator.statementToCode(block, 'OBJECTS_2');
 
   return `
 (function() {
-  const intersectGroup = new THREE.Group();
-  const parentGroup = sceneGroup;
-  sceneGroup = intersectGroup;
-  ${statement}
+  const group1 = new THREE.Group();
+  const group2 = new THREE.Group();
+  let parentGroup = sceneGroup;
+
+  sceneGroup = group1;
+  ${statement1}
+
+  sceneGroup = group2;
+  ${statement2}
+
   sceneGroup = parentGroup;
-  sceneGroup.add(intersectGroup);
+
+  const intersectionGroup = new THREE.Group();
+
+  // Simulasi Visual Irisan (Intersection Highlight)
+  group1.traverse(child => {
+    if (child.isMesh) {
+      child.material = child.material.clone();
+      child.material.transparent = true;
+      child.material.opacity = 0.7;
+    }
+  });
+
+  group2.traverse(child => {
+    if (child.isMesh) {
+      child.material = child.material.clone();
+      child.material.wireframe = true;
+      child.material.color.setHex(0x00ffff);
+    }
+  });
+
+  intersectionGroup.add(group1);
+  intersectionGroup.add(group2);
+
+  sceneGroup.add(intersectionGroup);
 })();
 `;
 };
